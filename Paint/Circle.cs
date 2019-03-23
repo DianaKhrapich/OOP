2643e84b5a975ebe72c97a1083415bd72bbe2653
Diana <apple301999@gmail.com>
2019-03-23 08:28:09 +0300

    Transfer project to WPF.

---
+++ Paint/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Paint
+{
+    class Circle : Shape
+    {
+        public override StylusPointCollection GetPoints(Point anchor, Point cursor)
+        {
+            var pointCollection = new StylusPointCollection();
+
+            return pointCollection;
+        }
+    }
+}
---
+++ Paint/Shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Paint
+{
+    abstract class Shape
+    {
+        public abstract StylusPointCollection GetPoints(Point anchor, Point cursor);
+    }
+}
